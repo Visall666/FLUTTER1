ba501df6411e1f3691c4d9fefb32a1d0d076a959
Visal Kong <viisall59@gmail.com>
2025-10-14 23:30:12 +0700

    Create EX-1.dart

---
+++ W1/Dart Basics PART 2/EX-1.dart
@@ -0,0 +1,8 @@
+void main() {
+  // List of student scores
+  List<int> scores = [45, 67, 82, 49, 51, 78, 92, 30];
+
+  // You code
+  var pass_scores = scores.where((score) => score >= 50);
+  print(pass_scores.toList());
+}
