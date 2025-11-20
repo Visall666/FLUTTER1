c6618a9e34a4ea8771fadf4f60b7152cf67e5213
Visal KONG <viisall59@gmail.com>
2025-11-20 22:58:44 +0700

    Add ex_3

---
+++ ex-3.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+List<String> images = [
+  "assets/w4-s2/bird.jpg",
+  "assets/w4-s2/bird2.jpg",
+  "assets/w4-s2/insect.jpg",
+  "assets/w4-s2/girl.jpg",
+  "assets/w4-s2/man.jpg",
+];
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false, 
+    home: const ImageViewer(),
+  ),
+);
+
+class ImageViewer extends StatefulWidget {
+  const ImageViewer({super.key});
+
+  @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  int index = 0;
+
+  void previousImage() {
+    setState(() {
+      index = (index - 1 + images.length) % images.length;
+    });
+  }
+
+  void nextImage() {
+    setState(() {
+      index = (index + 1) % images.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: previousImage,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: nextImage,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Image.asset(
+          images[index],
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
